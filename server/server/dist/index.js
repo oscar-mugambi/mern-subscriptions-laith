157ffea4ce4fe86086c22130d740f77a8c6f2485
oscar-mugambi <oscarmugambi@gmail.com>
2022-01-17 19:53:54 +0300

    added auth middleware

--- server/server/dist/index.js
+++ server/server/dist/index.js
@@ -4,7 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const auth_1 = __importDefault(require("./routes/auth"));
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use('/auth', auth_1.default);
+app.get('/', (_req, res) => {
+    res.send('hello world');
+});
 app.listen(4000, () => {
     console.log('running on port 4000');
 });
--- server/server/src/index.ts
+++ server/server/src/index.ts
@@ -1,6 +1,15 @@
 import express from 'express';
+import authRoutes from './routes/auth';
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/auth', authRoutes);
+
+app.get('/', (_req, res) => {
+  res.send('hello world');
+});
 
 app.listen(4000, () => {
   console.log('running on port 4000');
