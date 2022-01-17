0f1e4637698210d6c43ae5f610067a3076347fca
oscar-mugambi <oscarmugambi@gmail.com>
2022-01-17 21:00:27 +0300

    added user schema

---
+++ server/src/models/User.ts
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 5,
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
